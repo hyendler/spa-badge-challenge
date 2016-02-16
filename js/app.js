0188baee3369445953590fc5b2b70a4949971261
Hanah Yendler <email": null>
2016-02-16 12:43:45 -0800

    Add handlebars template for front page

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
-$document.ready(function(){
-
+$(document).ready(function(){
 
     var theTemplateScript = $("#all-students").html();
-
     var theTemplate = Handlebars.compile(theTemplateScript);
+    var context = {};
 
-    $.get({"students/"
+    $.get("http://localhost:3000/students")
+      .done(function(response){
+        context = {
+          students: response
+        }
+        var theCompiledHtml = theTemplate(context);
+        console.log(theCompiledHtml);
+        $(".content-placeholder").append(theCompiledHtml);
     })
 
+    bindListeners();
 
 });
+
+
+var bindListeners = function(){
+  $("container").("submit", "list-student a", displayStudent)
+}
+
+var displayStudent = function(e){
+  e.preventDefault();
+}
